5250906776e5a5d769a0ea4e2087ec5567166caa
AlexR <rozenbergalx@gmail.com>
2023-03-16 01:27:16 +0100

    some changes to routes

--- models/facility-model.js
+++ models/facility-model.js
@@ -4,8 +4,13 @@ const FacilitySchema = new mongoose.Schema({
   facilityName: { type: String, unique: true },
   sku: { type: String, unique: true },
   standard: { type: String },
-  status: { type: String },
   manufacturer: { type: String },
+  manufacturerType: { type: String },
+  basis: { type: String },
+  imageUrl: {
+    type: String,
+    default: 'https://cdn.pixabay.com/photo/2017/04/26/21/22/playground-2263827_960_720.jpg',
+  },
   isFailed: { type: Boolean, default: false },
   issueDescription: { type: String },
 });
